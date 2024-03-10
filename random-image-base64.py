fadd3292c721316b114449c29e839fe20a05e831
flattumc <122336698+flattumc@users.noreply.github.com>
2024-03-10 15:36:04 -0500

    Add files via upload

---
+++ random-image-base64.py
@@ -0,0 +1,23 @@
+from flask import Flask, jsonify
+import requests
+from PIL import Image
+from io import BytesIO
+import base64
+
+app = Flask(__name__)
+
+@app.route('/api/random-image-base64')
+def random_image_base64():
+    url = 'https://microservice-image-generator-bd1f70093a8a.herokuapp.com/random-image'
+    response = requests.get(url)
+    if response.status_code == 200:
+        image = Image.open(BytesIO(response.content))
+        buffered = BytesIO()
+        image.save(buffered, format="JPEG")
+        img_str = base64.b64encode(buffered.getvalue()).decode()
+        return jsonify(img_str)
+    else:
+        return jsonify(error="Failed to fetch image"), 500
+
+if __name__ == '__main__':
+    app.run(debug=True)
